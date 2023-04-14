edef3de8707fb5c7d7ed8006f34480b810718a41
odabasi61 <aliodabas_a@hotmail.com>
2023-04-15 02:08:30 +0300

    photo section finished

--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -1,30 +1,72 @@
 import { gallery } from '../helper/photo';
 import React, { useState, useEffect } from 'react';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { BsChevronLeft, BsChevronRight} from 'react-icons/bs';
 
 const Photo = () => {
-  const [shuffledImages, setShuffledImages] = useState(() => {
-    // shuffle the images array using Fisher-Yates algorithm
-    const shuffledArray = [...gallery];
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-    }
-    return shuffledArray;
-  });
+  // const [shuffledImages, setShuffledImages] = useState(() => {
+  //   // shuffle the images array using Fisher-Yates algorithm
+  //   const shuffledArray = [...gallery];
+  //   for (let i = shuffledArray.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  //   }
+  //   return shuffledArray;
+  // });
 
-  // with the function up, everytimes the page is refreshed, images are mixed.
+  // with the function up, everytimes the page is refreshed, images are mixed. when using the function, (shuffledImages) must be mapped
+
+  // with the function down, we make the selested images fullscreen.
+
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+
+  const handleImageClick = (index) => {
+    setSelectedImageIndex(index);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImageIndex(null);
+  };
+
+  const handlePrevClick = () => {
+    setSelectedImageIndex((prevIndex) => {
+      if (prevIndex === null) {
+        return null;
+      }
+      return prevIndex > 0 ? prevIndex - 1 : gallery.length - 1;
+    });
+  };
+
+  const handleNextClick = () => {
+    setSelectedImageIndex((prevIndex) => {
+      if (prevIndex === null) {
+        return null;
+      }
+      return prevIndex < gallery.length - 1 ? prevIndex + 1 : 0;
+    });
+  };
 
   return (
     <div className="p-2 p-md-3 p-lg-4 p-xl-5">
       <div className="p-2 bg-white rounded-2 d-flex flex-wrap justify-content-center gap-2">
-        {shuffledImages.map((photo, index) => (
+        {gallery.map((photo, index) => (
           <div key={index}>
             <div className='gallery-card rounded-4 overflow-hidden'>
-            <img src={photo} alt='beautiful landscape from Sinop' />
+            <img onClick={() => handleImageClick(index)} src={photo} alt='beautiful landscape from Sinop' />
           </div>
           </div>
         ))}
       </div>
+      {selectedImageIndex !== null && (
+        <div className="modal">
+          <div className="modal-content">
+            <img src={gallery[selectedImageIndex]} alt="beautiful landscape from Sinop" />
+            <button className='prev' onClick={handlePrevClick}><BsChevronLeft size={40} /></button>
+            <button className='next' onClick={handleNextClick}><BsChevronRight size={40} /></button>
+            <button className='close' onClick={handleCloseModal}><AiFillCloseCircle size={30} /></button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
